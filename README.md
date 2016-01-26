4b3c65790f00776fa00cea296601160e00390470
ivan_kharin <lavriktm@mail.ru>
2016-01-26 11:43:51 +0300

    description and sql

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # python_ipcam_screen_analyser
+Task - store motion detection sccrenshot from ip cam
+
+put this file into /var/www/mailanalyzer
+
+we use postfix for Mail Queue
+
+ipcam smtp settings:
+smtp : yoursmtp server
+tomail: youmailbox@yoursmtp server
+user: alert
+
+postfix alias file:
+alert: "|python /var/www/mailanalyzer/mail.py >> /var/www/mailanalyzer/log"
+
+
+
+
+
+
---
+++ cams_event.sql
@@ -0,0 +1,33 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `cams_event`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `event`
+--
+
+CREATE TABLE IF NOT EXISTS `event` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `cam` varchar(16) NOT NULL,
+  `chanel` int(2) NOT NULL,
+  `type` enum('motion','nomer','','') NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `data` varchar(25) NOT NULL,
+  `screen` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2114760 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
